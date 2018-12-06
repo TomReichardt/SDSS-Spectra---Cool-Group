168f6079d3f1f010892abd24b306866a95801c73
theghostoutside <adriano.poci@students.mq.edu.au>
2018-12-06 13:34:43 +1100

    Added `plot_indices` method.

--- spectrum.py
+++ spectrum.py
@@ -5,7 +5,7 @@ from    os.path             import  isdir   as      opid
 from    astropy.io          import  fits
 from    glob                import  glob
 import  astropy.coordinates as      coord
-import  astropy.units       as      u
+import  astropy.units       as      uts
 import  matplotlib.pyplot   as      plt
 import  matplotlib.gridspec as      gridspec
 import  numpy               as      np
@@ -35,9 +35,9 @@ class Spectrum():
 
 
     def plot_on_sky(self, ax=None):
-        ra = coord.Angle(self.primary["RA"] * u.degree)
-        ra = ra.wrap_at(180 * u.degree)
-        dec = coord.Angle(self.primary["DEC"] * u.degree)
+        ra = coord.Angle(self.primary["RA"] * uts.degree)
+        ra = ra.wrap_at(180 * uts.degree)
+        dec = coord.Angle(self.primary["DEC"] * uts.degree)
 
         if ax is None:
             ax = plt.gca()
@@ -45,15 +45,35 @@ class Spectrum():
 
         return ax
 
-    def plot_indices(self):
+    def plot_indices(self, ax=None):
         lineWV    = self.spzline[  'LINEWAVE']
         lineNames = self.spzline[  'LINENAME']
         lineEW    = self.spzline[    'LINEEW']
         lineEWErr = self.spzline['LINEEW_ERR']
+        
+        sore = np.argsort( lineWV )
+        lineWV    =    lineWV[sore]
+        lineNames = lineNames[sore]
+        lineEW    =    lineEW[sore]
+        lineEWErr = lineEWErr[sore]
+        
+        if ax is None:
+            ax = plt.gca()
+        ax.errorbar( lineWV, lineEW, yerr=lineEWErr, fmt='o' )
+        ax.set_xlabel( r"Wavelength $[{}]$".format(uts.Unit('angstrom').to_string('latex_inline').strip('$')) )
+        ax.set_ylabel( r"${{\rm EW}}\ [{}]$".format(uts.Unit('angstrom').to_string('latex_inline').strip('$')) )
+        
+        
+        return ax
 
 fileList = glob( opj( curdir, 'spectra', '*.fits' ) )
 c = 299792.458
-s = Spectrum( fileList[1] )
+SPEC = Spectrum( fileList[1] )
+
+plt.clf()
+ax = SPEC.plot_indices()
+plt.savefig( 'test' )
+pdb.set_trace()
 
 fig = plt.figure(figsize=(8,6))
 gs = gridspec.GridSpec(1,2)
