c02eab8a35edefeda44cf6e2433c4aed8b43629e
TomReichardt <thomas.reichardt@students.mq.edu.au>
2018-12-07 15:35:02 +1100

    okay, this time actually be wary of pytest

--- test_spectrum.py
+++ test_spectrum.py
@@ -7,22 +7,40 @@ from    glob                import  glob
 import  matplotlib.pyplot   as      plt
 import  numpy               as      np
 #import  spectrum
-from spectrum import Spectrum
+from    spectrum            import Spectrum
 #from spectrum import show_spectrum
 
-test_spec1='./spectra/spec-6055-56102-0008.fits'
+curdir = os.path.split(os.path.realpath(__file__))[0]
+
+test_file = opj(curdir, 'spectra', 'spec-6055-56102-0008.fits')
 
 def test_fits_exist():
-    s = Spectrum(test_spec1)
+    s = Spectrum(test_file)
     assert opif(s.file), 'File does not exist'
 
+def test_ra():
+    s = Spectrum(test_file)
+    np.testing.assert_almost_equal(s.ra, 212.641274)
 
+def test_dec():
+    s = Spectrum(test_file)
+    np.testing.assert_almost_equal(s.dec, 44.621874)
 
-def test_coordinate():
-    s = Spectrum(test_spec1)
-    assert s.plot_on_sky
+def test_headers_read():
+    s = Spectrum(test_file)
+    assert len(s.headers) == 4, 'Unexpected number of headers: len(headers) != 4'
 
+def test_coadd():
+    s = Spectrum(test_file)
+    assert type(s.coadd) == fits.fitsrec.FITS_rec, 'COADD is the wrong object type or does not exist'
 
+def test_spall():
+    s = Spectrum(test_file)
+    assert type(s.spall) == fits.fitsrec.FITS_rec, 'SPALL is the wrong object type or does not exist'
 
+def test_spall():
+    s = Spectrum(test_file)
+    assert type(s.spzline) == fits.fitsrec.FITS_rec, 'SPALL is the wrong object type or does not exist'
 
-
+s = Spectrum(test_file)
+print(type(s.spzline))
